4b8df05d911d1ca2cf1039e5876c857d28fe7f2d
khaledsaeedcr7 <khaledsaeedcr7@gmail.com>
2023-12-22 15:58:32 +0200

    mange section use email

--- review.php
+++ review.php
@@ -1,5 +1,9 @@
 <?php
-
+session_start();
+if (!isset($_SESSION['email'])) {
+  header("Location: login.php");
+  exit();
+}
 $con = mysqli_connect("localhost", "root", "", "yummy") or die("connection failed");
 
 
